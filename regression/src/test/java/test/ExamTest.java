b5e095c764f4dca6426e2cc022e2b7d1ac979d22
Andreas Pieber <anpieber@gmail.com>
2011-09-03 08:01:15 +0200

    Finally added regression test
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

---
+++ regression/src/test/java/test/ExamTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.ExamReactorStrategy;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.options.CustomFrameworkOption;
+import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
+
+@RunWith(JUnit4TestRunner.class)
+@ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
+public class ExamTest {
+
+    @Configuration
+    public Option[] config() {
+        return new Option[]{ new CustomFrameworkOption("mvn:org.apache.karaf/apache-karaf/2.2.3/zip", "karaf",
+            "karaf-2.2.3") };
+    }
+
+    @Test
+    public void test() throws Exception {
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+    }
+}
