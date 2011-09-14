65641853f3111a654cc248f9ca32737742e401be
Andreas Pieber <anpieber@gmail.com>
2011-09-14 05:21:50 +0200

    [LAB-48] Fixed eclipse problem
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

---
+++ regression/src/test/java/org/openengsb/labs/paxexam/karaf/regression/VersionAsInProjectOpenEngSBTest.java
@@ -0,0 +1,44 @@
+package org.openengsb.labs.paxexam.karaf.regression;
+
+import static junit.framework.Assert.assertTrue;
+import static org.openengsb.labs.paxexam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
+import static org.ops4j.pax.exam.CoreOptions.maven;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.ExamReactorStrategy;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
+
+/**
+ * This test should validate if the versionAsInProject tags work as expected.
+ */
+@RunWith(JUnit4TestRunner.class)
+@ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
+public class VersionAsInProjectOpenEngSBTest {
+
+    @Configuration
+    public Option[] config() {
+        return new Option[]{ karafDistributionConfiguration().frameworkUrl(
+            maven().groupId("org.openengsb").artifactId("openengsb").type("zip").versionAsInProject()) };
+    }
+
+    @Test
+    public void test() throws Exception {
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+        assertTrue(true);
+    }
+
+}
