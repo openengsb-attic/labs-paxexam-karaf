1b70502660fa667062e2aa9cace76fe59565622e
Andreas Pieber <anpieber@gmail.com>
2012-02-19 15:56:48 +0100

    Add ExamBundleStartLevel to KarafDistributionOptions
    For easier accessibility this patch adds the ExamBundleStartLevel option
    to the KarafDistributionOption static class to allow a direct
    referencing on it; this should make it easier to find and use those
    options.
    
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>
    (cherry picked from commit 3b2a4a87d3e986c43454fb9e9aff9cab666fb542)
    
    Conflicts:
    
    	options/src/main/java/org/openengsb/labs/paxexam/karaf/options/KarafDistributionOption.java
    
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- options/src/main/java/org/openengsb/labs/paxexam/karaf/options/KarafDistributionOption.java
+++ options/src/main/java/org/openengsb/labs/paxexam/karaf/options/KarafDistributionOption.java
@@ -46,6 +46,16 @@ public final class KarafDistributionOption {
     }
 
     /**
+
+    /**
+     * This option allows to configure the start level of the bundles in the exam features descriptor.
+     */
+    public static Option useOwnExamBundlesStartLevel(int startLevel) {
+        return new ExamBundlesStartLevel(startLevel);
+    }
+
+
+    /**
      * The karaf pax-logging configuration is typically not a file manipulated very often. Therefore we take the freedom
      * of adding a console logger and changing the log level directly. IF you like to configure the file manually (or
      * had so in your distribution) add this option to avoid any automatic modifications to this file!
